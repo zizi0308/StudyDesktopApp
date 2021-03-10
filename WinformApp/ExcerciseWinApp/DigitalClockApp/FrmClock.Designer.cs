461d6b3532892f28ca24a15e60c30a5aed40b919
JO HUI JI <whgmlwl222@naver.com>
2021-03-10 17:13:42 +0900

    20210310 커밋

---
+++ WinformApp/ExcerciseWinApp/DigitalClockApp/FrmClock.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace DigitalClockApp
+{
+    partial class FrmClock
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LblClock = new System.Windows.Forms.Label();
+            this.MyTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // LblClock
+            // 
+            this.LblClock.AutoSize = true;
+            this.LblClock.BackColor = System.Drawing.Color.Lavender;
+            this.LblClock.Font = new System.Drawing.Font("함초롬바탕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.LblClock.Location = new System.Drawing.Point(41, 25);
+            this.LblClock.Name = "LblClock";
+            this.LblClock.Size = new System.Drawing.Size(246, 32);
+            this.LblClock.TabIndex = 0;
+            this.LblClock.Text = "2021-03-10 16:17:20";
+            // 
+            // MyTimer
+            // 
+            this.MyTimer.Enabled = true;
+            this.MyTimer.Tick += new System.EventHandler(this.MyTimer_Tick);
+            // 
+            // FrmClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lavender;
+            this.ClientSize = new System.Drawing.Size(324, 85);
+            this.Controls.Add(this.LblClock);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmClock";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "디지털 시계";
+            this.Load += new System.EventHandler(this.FrmClock_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblClock;
+        private System.Windows.Forms.Timer MyTimer;
+    }
+}
+
