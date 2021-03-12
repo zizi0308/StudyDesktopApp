f48aaa21b47564039441f6aae5254849adaf0e21
JO HUI JI <whgmlwl222@naver.com>
2021-03-12 15:55:59 +0900

    210312 WinForm

---
+++ WinformApp/WinExecutiveBank/MyNotePadApp/MyNotePadApp/AboutThis.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyNotePadApp
+{
+    partial class AboutThis : Form
+    {
+        public AboutThis()
+        {
+            InitializeComponent();
+            this.Text = String.Format("{0} 정보", AssemblyTitle);
+            this.labelProductName.Text = AssemblyProduct;
+            this.labelVersion.Text = String.Format("버전 {0}", AssemblyVersion);
+            this.labelCopyright.Text = AssemblyCopyright;
+            this.labelCompanyName.Text = AssemblyCompany;
+            this.textBoxDescription.Text = AssemblyDescription;
+        }
+
+    }
+}
