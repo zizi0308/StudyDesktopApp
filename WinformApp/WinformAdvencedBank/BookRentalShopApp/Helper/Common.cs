f41fe8e96751296893d7fdee78b952869f1bd827
JO HUI JI <whgmlwl222@naver.com>
2021-03-16 17:27:56 +0900

    화요일 커밋

---
+++ WinformApp/WinformAdvencedBank/BookRentalShopApp/Helper/Common.cs
@@ -0,0 +1,13 @@
+﻿namespace BookRentalShopApp.Helper
+{
+    public class Common
+    {
+        public static string ConnString = "Data Source=127.0.0.1;" +
+                                        "Initial Catalog=bookrentalshop;" +
+                                        "Persist Security Info=True;" +
+                                        "User ID=SA;" +
+                                        "Password=msspl_p@ssw0rd!";
+
+        public static string LoginUserId = string.Empty;
+    }
+}
