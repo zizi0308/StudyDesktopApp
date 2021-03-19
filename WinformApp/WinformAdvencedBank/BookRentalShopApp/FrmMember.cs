df136de418369d35a3dd0186a51d7ba1d53cbaf8
JO HUI JI <whgmlwl222@naver.com>
2021-03-19 18:39:30 +0900

    BookRentalShop 최종 커밋

--- WinformApp/WinformAdvencedBank/BookRentalShopApp/FrmMember.cs
+++ WinformApp/WinformAdvencedBank/BookRentalShopApp/FrmMember.cs
@@ -1,14 +1,8 @@
 ﻿using MetroFramework;
 using MetroFramework.Forms;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BookRentalShopApp
@@ -16,12 +10,13 @@ namespace BookRentalShopApp
     public partial class FrmMember : MetroForm
     {
         #region 전역변수 영역
-        
-        private bool isNew = false;
+
+        private bool isNew = false; // false (수정) / true (신규)
 
         #endregion
 
         #region 이벤트 영역
+
         public FrmMember()
         {
             InitializeComponent();
@@ -29,7 +24,7 @@ namespace BookRentalShopApp
 
         private void FrmDivCode_Load(object sender, EventArgs e)
         {
-            isNew = true; // 초기화
+            isNew = true; // 신규 초기화
             RefreshData();
         }
 
@@ -46,16 +41,16 @@ namespace BookRentalShopApp
                 var selData = DgvData.Rows[e.RowIndex];
                 TxtIdx.Text = selData.Cells[0].Value.ToString();
                 TxtNames.Text = selData.Cells[1].Value.ToString();
-                CboLevels.SelectedItem = selData.Cells[2].Value.ToString();
+                CboLevels.SelectedItem = selData.Cells[2].Value;
 
                 TxtAddr.Text = selData.Cells[3].Value.ToString();
                 TxtMobile.Text = selData.Cells[4].Value.ToString();
                 TxtEmail.Text = selData.Cells[5].Value.ToString();
-                TxtuserID.Text = selData.Cells[6].Value.ToString();
+                TxtUserId.Text = selData.Cells[6].Value.ToString();
 
                 TxtIdx.ReadOnly = true;
 
-                isNew = false;
+                isNew = false;  // 수정
             }
         }
 
@@ -63,9 +58,8 @@ namespace BookRentalShopApp
         {
             // Validation
             if (CheckValidation() == false) return;
-
-            if (MetroMessageBox.Show(this, "삭제하시겠습니까?", "삭제",
-                MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No) return;
+            // 삭제질문
+            if (MetroMessageBox.Show(this, "삭제하시겠습니까?", "삭제", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.No) return;
 
             DeleteData();
             RefreshData();
@@ -81,7 +75,6 @@ namespace BookRentalShopApp
         {
             // Validation 체크
             if (CheckValidation() == false) return;
-
 
             SaveData();
             RefreshData();
@@ -107,24 +100,24 @@ namespace BookRentalShopApp
 
                     var query = "";
 
-                    query = "DELETE FROM [dbo].[divtbl] " +
+                    query = "DELETE FROM [dbo].[membertbl] " +
                             " WHERE [Idx] = @Idx ";
                     cmd.CommandText = query;
 
-                    SqlParameter pIdx = new SqlParameter("@Idx", SqlDbType.Int);
+                    var pIdx = new SqlParameter("@Idx", SqlDbType.Int);
                     pIdx.Value = TxtIdx.Text;
                     cmd.Parameters.Add(pIdx);
 
                     var result = cmd.ExecuteNonQuery();
                     if (result == 1)
                     {
-                        MetroMessageBox.Show(this, "삭제성공", "삭제",
+                        MetroMessageBox.Show(this, "삭제 성공", "삭제",
                             MessageBoxButtons.OK, MessageBoxIcon.Information);
                     }
                     else
                     {
-                        MetroMessageBox.Show(this, "삭제실패", "삭제",
-                           MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        MetroMessageBox.Show(this, "삭제 실패", "삭제",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     }
                 }
             }
@@ -135,108 +128,6 @@ namespace BookRentalShopApp
             }
         }
 
-        private void SaveData()
-        {
-            try
-            {
-                using (SqlConnection conn = new SqlConnection(Helper.Common.ConnString))
-                {
-                    if (conn.State == ConnectionState.Closed) conn.Open();
-                    SqlCommand cmd = new SqlCommand();
-                    cmd.Connection = conn;
-
-                    var query = "";
-
-                    if (isNew) // INSERT
-                    {
-                        query = @"INSERT INTO [dbo].[membertbl]
-                                           ([Names]
-                                           ,[Levels]
-                                           ,[Addr]
-                                           ,[Mobile]
-                                           ,[Email]
-                                           ,[userID]
-                                           ,[passwords])
-                                     VALUES
-                                           (@Names
-                                           ,@Levels
-                                           ,@Addr
-                                           ,@Mobile
-                                           ,@Email
-                                           ,@userID
-                                           ,@passwords) ";
-                    }
-                    else // UPDATE
-                    {
-                        query = @"UPDATE [dbo].[membertbl]
-                                       SET [Names] = @Names
-                                          ,[Levels] = @Levels
-                                          ,[Addr] = @Addr
-                                          ,[Mobile] = @Mobile
-                                          ,[Email] = @Email
-                                          ,[userID] = @userID
-                                          ,[passwords] = @passwords
-                                     WHERE Idx = @Idx;";
-                    }
-
-                    cmd.CommandText = query;
-
-                    SqlParameter pNames = new SqlParameter("@Names", SqlDbType.NVarChar, 50);
-                    pNames.Value = TxtNames.Text;
-                    cmd.Parameters.Add(pNames);
-
-                    SqlParameter pLevels = new SqlParameter("@Levels", SqlDbType.Char, 1);
-                    pLevels.Value = CboLevels.SelectedItem.ToString();
-                    cmd.Parameters.Add(pLevels);
-
-                    SqlParameter pAddr = new SqlParameter("@Addr", SqlDbType.NVarChar, 100);
-                    pAddr.Value = TxtAddr.Text;
-                    cmd.Parameters.Add(pAddr);
-
-                    SqlParameter pMobile = new SqlParameter("@Mobile", SqlDbType.VarChar, 13);
-                    pMobile.Value = TxtMobile.Text;
-                    cmd.Parameters.Add(pMobile);
-
-                    SqlParameter pEmail = new SqlParameter("@Email", SqlDbType.VarChar, 50);
-                    pEmail.Value = TxtEmail.Text;
-                    cmd.Parameters.Add(pEmail);
-
-                    SqlParameter pUserID = new SqlParameter("@userID", SqlDbType.VarChar, 20);
-                    pUserID.Value = TxtuserID.Text;
-                    cmd.Parameters.Add(pUserID);
-
-                    SqlParameter pPassword = new SqlParameter("@passwords", SqlDbType.VarChar, 100);
-                    pPassword.Value = TxtPassword.Text;
-                    cmd.Parameters.Add(pPassword);
-
-                    if (isNew == false) // UPDATE
-                    {
-                        var pIdx = new SqlParameter("@Idx", SqlDbType.Int);
-                        pIdx.Value = TxtIdx.Text;
-                        cmd.Parameters.Add(pIdx);
-                    }
-
-
-                    var result = cmd.ExecuteNonQuery();
-                    if (result == 1)
-                    {
-                        MetroMessageBox.Show(this, "저장성공", "저장",
-                            MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    }
-                    else
-                    {
-                        MetroMessageBox.Show(this, "저장실패", "저장",
-                           MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MetroMessageBox.Show(this, $"예외발생 : {ex.Message}", "오류", MessageBoxButtons.OK,
-                    MessageBoxIcon.Error);
-            }
-        }
-       
         private void RefreshData()
         {
             try
@@ -245,16 +136,16 @@ namespace BookRentalShopApp
                 {
                     if (conn.State == ConnectionState.Closed) conn.Open();
 
-                    var query = @" SELECT [Idx]
-                                       ,[Names]
-                                       ,[Levels]
-                                       ,[Addr]
-                                       ,[Mobile]
-                                       ,[Email]
-                                       ,[userID]
-                                       ,[lastLoginDt]
-                                       ,[loginIpAddr]
-                                    FROM [dbo].[membertbl] ";
+                    var query = @"SELECT [Idx]
+                                      ,[Names]
+                                      ,[Levels]
+                                      ,[Addr]
+                                      ,[Mobile]
+                                      ,[Email]
+                                      ,[userID]                                      
+                                      ,[lastLoginDt]
+                                      ,[loginIpAddr]
+                                  FROM [dbo].[membertbl] ";
                     SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
                     DataSet ds = new DataSet();
                     adapter.Fill(ds, "membertbl");
@@ -270,14 +161,107 @@ namespace BookRentalShopApp
             }
         }
 
-        private void ClearInputs()
+        /// <summary>
+        /// 입력(수정)처리 프로세스
+        /// </summary>
+        private void SaveData()
         {
-            TxtIdx.Text = TxtNames.Text = TxtuserID.Text = "";
-            TxtMobile.Text = TxtAddr.Text = TxtEmail.Text = "";
-            TxtPassword.Text = "";
-            CboLevels.SelectedIndex = -1;
-            TxtIdx.ReadOnly = true;
-            isNew = true;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(Helper.Common.ConnString))
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.Connection = conn;
+
+                    var query = "";
+
+                    if (isNew == true) // ISNERT
+                    {
+                        query = @"INSERT INTO [dbo].[membertbl]
+                                       ([Names]
+                                       ,[Levels]
+                                       ,[Addr]
+                                       ,[Mobile]
+                                       ,[Email]
+                                       ,[userID]
+                                       ,[passwords])
+                                 VALUES
+                                       (@Names
+                                       ,@Levels
+                                       ,@Addr
+                                       ,@Mobile
+                                       ,@Email
+                                       ,@userID
+                                       ,@passwords) ";
+                    }
+                    else // UPDATE
+                    {
+                        query = @"UPDATE [dbo].[membertbl]
+                                       SET [Names] = @Names
+                                          ,[Levels] = @Levels
+                                          ,[Addr] = @Addr
+                                          ,[Mobile] = @Mobile
+                                          ,[Email] = @Email
+                                          ,[userID] = @userID
+                                          ,[passwords] = @passwords
+                                     WHERE Idx = @Idx ";
+                    }
+                    cmd.CommandText = query;
+
+                    var pNames = new SqlParameter("@Names", SqlDbType.NVarChar, 50);
+                    pNames.Value = TxtNames.Text;
+                    cmd.Parameters.Add(pNames);
+
+                    var pLevels = new SqlParameter("@Levels", SqlDbType.Char, 1);
+                    pLevels.Value = CboLevels.SelectedItem.ToString();
+                    cmd.Parameters.Add(pLevels);
+
+                    var pAddr = new SqlParameter("@Addr", SqlDbType.NVarChar, 100);
+                    pAddr.Value = TxtAddr.Text;
+                    cmd.Parameters.Add(pAddr);
+
+                    var pMobile = new SqlParameter("@Mobile", SqlDbType.VarChar, 13);
+                    pMobile.Value = TxtMobile.Text;
+                    cmd.Parameters.Add(pMobile);
+
+                    var pEmail = new SqlParameter("@Email", SqlDbType.VarChar, 50);
+                    pEmail.Value = TxtEmail.Text;
+                    cmd.Parameters.Add(pEmail);
+
+                    var pUserId = new SqlParameter("@userId", SqlDbType.VarChar, 20);
+                    pUserId.Value = TxtUserId.Text;
+                    cmd.Parameters.Add(pUserId);
+
+                    var pPasswords = new SqlParameter("@passwords", SqlDbType.VarChar, 100);
+                    pPasswords.Value = TxtPasswords.Text;
+                    cmd.Parameters.Add(pPasswords);
+
+                    if (isNew == false) // Update 일때만 처리
+                    {
+                        var pIdx = new SqlParameter("@Idx", SqlDbType.Int);
+                        pIdx.Value = TxtIdx.Text;
+                        cmd.Parameters.Add(pIdx);
+                    }
+
+                    var result = cmd.ExecuteNonQuery();
+                    if (result == 1)
+                    {
+                        MetroMessageBox.Show(this, "저장 성공", "저장",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MetroMessageBox.Show(this, "저장 실패", "저장",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MetroMessageBox.Show(this, $"예외발생 : {ex.Message}", "오류", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
         }
 
         /// <summary>
@@ -286,10 +270,10 @@ namespace BookRentalShopApp
         /// <returns></returns>
         private bool CheckValidation()
         {
-            if (string.IsNullOrEmpty(TxtIdx.Text) || string.IsNullOrEmpty(TxtNames.Text) || 
-                string.IsNullOrEmpty(TxtAddr.Text) || string.IsNullOrEmpty(TxtMobile.Text) || 
-                string.IsNullOrEmpty(TxtEmail.Text) || CboLevels.SelectedIndex == -1 || string.IsNullOrEmpty(TxtuserID.Text))
-
+            if (string.IsNullOrEmpty(TxtNames.Text) ||
+                string.IsNullOrEmpty(TxtAddr.Text) || string.IsNullOrEmpty(TxtMobile.Text) ||
+                string.IsNullOrEmpty(TxtEmail.Text) || CboLevels.SelectedIndex == -1 ||
+                string.IsNullOrEmpty(TxtUserId.Text))
             {
                 MetroMessageBox.Show(this, "빈값은 처리할 수 없습니다.", "경고",
                     MessageBoxButtons.OK, MessageBoxIcon.Warning);
@@ -297,9 +281,18 @@ namespace BookRentalShopApp
             }
 
             return true;
-        
         }
-        
-        #endregion     
+
+        private void ClearInputs()
+        {
+            TxtIdx.Text = TxtNames.Text = "";
+            TxtMobile.Text = TxtAddr.Text = TxtEmail.Text = "";
+            TxtUserId.Text = TxtPasswords.Text = "";
+            CboLevels.SelectedIndex = -1; // ?
+            TxtIdx.ReadOnly = true;
+            isNew = true;
+        }
+
+        #endregion
     }
 }
