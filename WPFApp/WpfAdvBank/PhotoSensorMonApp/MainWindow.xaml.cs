d83c8332347a94341d2fbfe8869879932322fbaa
JO HUI JI <whgmlwl222@naver.com>
2021-03-29 17:33:36 +0900

    수정 커밋

--- WPFApp/WpfAdvBank/PhotoSensorMonApp/MainWindow.xaml.cs
+++ WPFApp/WpfAdvBank/PhotoSensorMonApp/MainWindow.xaml.cs
@@ -1,25 +1,11 @@
 ﻿using LiveCharts;
-using LiveCharts.Wpf;
 using MahApps.Metro.Controls;
 using OxyPlot;
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace PhotoSensorMonApp
